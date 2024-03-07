e3279d816d3d8474309bc6c5da3bdc4b95d5cf21
Bikram Upadhyay <upadhyaybikram@gmail.com>
2024-03-07 20:46:52 +1100

    #services retrieved from the modal #appointments created

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Appointment;
-use App\Models\Barber;
 use App\Models\Customer;
 use App\Models\Service;
 use Carbon\CarbonPeriod;
@@ -16,7 +15,7 @@ class AppointmentController extends Controller
     public function showBookingForm()
     {
         $services = Service::all();
-        $period = new CarbonPeriod('10:00', '30 minutes', '20:00'); // for create use 24 hours format later change format
+        $period = new CarbonPeriod('10:00', '30 minutes', '20:00');
         $slots = [];
         foreach($period as $item){
             array_push($slots,$item->format("h:i A"));
@@ -26,8 +25,6 @@ class AppointmentController extends Controller
 
     public function bookAppointment(Request $request)
     {
-     //   dd($request->input());
-
         $request->validate([
             'service' => 'required|exists:services,id',
             'datepicker' => 'required|date',
@@ -40,31 +37,22 @@ class AppointmentController extends Controller
 
         DB::beginTransaction();
 
-        $customer = Customer::firstOrCreate(
-            ['mobile' => $request->mobile, 'email' => $request->email],
-            [
-                'first_name' => $request->customer_first_name,
-                'last_name' => $request->customer_last_name,
+        $customer = Customer::firstOrCreate([
+            'mobile' => $request->mobile,
+            'email' => $request->email,
+            'first_name' => $request->customer_first_name,
+            'last_name' => $request->customer_last_name,
             ]
         );
         $time = Carbon::createFromFormat('h:i A', $request->timeSlot)->format('H:i:s');
-
-        // Create an appointment
         $appointment = Appointment::create([
             'customer_id' => $customer->id,
             'branch' => 'Hurstville',
             'date' => $request->datepicker,
             'time' => $time
         ]);
-
-        // Attach the selected service types to the appointment
         $appointment->serviceTypes()->attach($request->service);
-
         DB::commit();
-        // You may want to send a confirmation email or perform other actions here
-
-
         return redirect()->back()->with('success', 'Appointment booked successfully!');
-
     }
 }
